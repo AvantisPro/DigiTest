7108c74f9bb3958bf14b6998346a77aa8905b7ba
Vlad <proyava2296@gmail.com>
2020-11-04 14:43:46 +0200

    new tests

--- src/test/java/Admin_panel.java
+++ src/test/java/Admin_panel.java
@@ -28,6 +28,7 @@ public class Admin_panel {
 
     @BeforeClass
     public void init() throws IOException {
+        System.out.println(" ");
         service.start();
         driver = new RemoteWebDriver(service.getUrl(), new ChromeOptions());
         driver.manage().window().maximize();
