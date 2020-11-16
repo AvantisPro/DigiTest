c6294944fd41e8f57187c1afb15b8b21f0d0c004
Vlad <proyava2296@gmail.com>
2020-11-16 11:58:12 +0200

    new tests and fix 1156

--- src/test/java/Brand2.java
+++ src/test/java/Brand2.java
@@ -20,8 +20,8 @@ public class Brand2 {
     ChromeDriverService service = new ChromeDriverService.Builder().usingPort(8082).
             usingDriverExecutable(new File("/usr/bin/chromedriver"))
             .build();
-    //
 
+    //
     @BeforeClass
     public void Init() throws IOException {
         service.start();
